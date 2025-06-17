89985e31e68512493e5c487944043c43e3d40cc4
Neva <nevao@uw.edu>
2025-06-17 16:20:52 -0700

    Calculate statistical difference between sexes

--- AdolBehavCalcNormativeModelCOVID_AllsubjwithBehav.py
+++ AdolBehavCalcNormativeModelCOVID_AllsubjwithBehav.py
@@ -16,6 +16,7 @@ from plot_and_compute_zdistributions import plot_by_gender_no_kde, plot_and_comp
 from calc_ef_stats import calc_ef_stats
 
 ######Variables to specify#############
+run_fit = 0
 n_splits = 100
 show_plots = 0  #set to 1 to show training and test data y vs yhat and spline fit plots. Set to 0 to save to file.
 show_nsubject_plots = 0 #set to 1 to show number of subjects in analysis
@@ -25,122 +26,134 @@ outputdirname = '/home/toddr/neva/PycharmProjects/AdolNormativeModelingCOVID'
 struct_var = 'behav'
 #######################################
 
-############## Load brain structural measures and behavior data and reorganize  ############
-all_data = load_data_aff_cog()
-columns_to_keep_meltzoff = ['subject', 'visit', 'gender', 'agemonths', 'agedays', 'agegroup', 'peermindset', 'persmindset',
-                   'needforapproval', 'needforbelonging', 'CDImean', 'RSQanxiety' ,'RSQanger',
-                   'Rejection', 'RejectCoping', 'RejectCopingSad', 'Coping_mad', 'Coping_sad', 'Coping_worried',
-                   'StateAnxiety', 'TraitAnxiety', 'FlankerSU','DCSU', 'VocabSU', 'WMemorySU']
+if run_fit:
+    ############## Load brain structural measures and behavior data and reorganize  ############
+    all_data = load_data_aff_cog()
+    columns_to_keep_meltzoff = ['subject', 'visit', 'gender', 'agemonths', 'agedays', 'agegroup', 'peermindset', 'persmindset',
+                       'needforapproval', 'needforbelonging', 'CDImean', 'RSQanxiety' ,'RSQanger',
+                       'Rejection', 'RejectCoping', 'RejectCopingSad', 'Coping_mad', 'Coping_sad', 'Coping_worried',
+                       'StateAnxiety', 'TraitAnxiety', 'FlankerSU','DCSU', 'VocabSU', 'WMemorySU']
 
-#remove structural and social media data
-dem_behav_data = all_data.loc[:, columns_to_keep_meltzoff].copy()
+    #remove structural and social media data
+    dem_behav_data = all_data.loc[:, columns_to_keep_meltzoff].copy()
 
-# Write subjects and nih toolkit scores to file
-v1andv2_subjects_behav = dem_behav_data[['subject', 'visit', 'agedays', 'gender', 'FlankerSU', 'DCSU', 'VocabSU', 'WMemorySU']]
-v1andv2_subjects_behav.to_csv('visit1andvisit2_subjects_behav.csv', index=False)
+    # Write subjects and nih toolkit scores to file
+    v1andv2_subjects_behav = dem_behav_data[['subject', 'visit', 'agedays', 'gender', 'FlankerSU', 'DCSU', 'VocabSU', 'WMemorySU']]
+    v1andv2_subjects_behav.to_csv('visit1andvisit2_subjects_behav.csv', index=False)
 
-#replace gender codes 1=male 2=female with binary values (make male=1 and female=0)
-dem_behav_data.loc[dem_behav_data['gender']==2, 'gender'] = 0
+    #replace gender codes 1=male 2=female with binary values (make male=1 and female=0)
+    dem_behav_data.loc[dem_behav_data['gender']==2, 'gender'] = 0
 
-unique_subjects = dem_behav_data['subject'].value_counts()
-unique_subjects = unique_subjects[unique_subjects == 1].index
-subjects_with_one_dataset = dem_behav_data[dem_behav_data['subject'].isin(unique_subjects)]
-subjects_visit1_data_only = subjects_with_one_dataset[subjects_with_one_dataset['visit'] == 1]
-subjects_visit2_data_only = subjects_with_one_dataset[subjects_with_one_dataset['visit'] == 2]
-subjects_v1_only = subjects_visit1_data_only['subject'].tolist()
-subjects_v2_only = subjects_visit2_data_only['subject'].tolist()
-all_subjects = dem_behav_data['subject'].unique().tolist()
-all_subjects_2ts = [sub for sub in all_subjects if (sub not in subjects_v1_only and sub not in subjects_v2_only)]
+    unique_subjects = dem_behav_data['subject'].value_counts()
+    unique_subjects = unique_subjects[unique_subjects == 1].index
+    subjects_with_one_dataset = dem_behav_data[dem_behav_data['subject'].isin(unique_subjects)]
+    subjects_visit1_data_only = subjects_with_one_dataset[subjects_with_one_dataset['visit'] == 1]
+    subjects_visit2_data_only = subjects_with_one_dataset[subjects_with_one_dataset['visit'] == 2]
+    subjects_v1_only = subjects_visit1_data_only['subject'].tolist()
+    subjects_v2_only = subjects_visit2_data_only['subject'].tolist()
+    all_subjects = dem_behav_data['subject'].unique().tolist()
+    all_subjects_2ts = [sub for sub in all_subjects if (sub not in subjects_v1_only and sub not in subjects_v2_only)]
 
-#make separate dataframes for visit 1 and visit 2
-dem_behav_data_v1 = dem_behav_data.loc[dem_behav_data['visit']==1]
-dem_behav_data_v2 = dem_behav_data.loc[dem_behav_data['visit']==2]
+    #make separate dataframes for visit 1 and visit 2
+    dem_behav_data_v1 = dem_behav_data.loc[dem_behav_data['visit']==1]
+    dem_behav_data_v2 = dem_behav_data.loc[dem_behav_data['visit']==2]
 
-#need to make cognitive numbers smaller or normative modeling function does not work
-cog_columns = ['FlankerSU', 'DCSU', 'VocabSU', 'WMemorySU']
-dem_behav_data_v1.loc[:,cog_columns] = dem_behav_data_v1.loc[:, cog_columns]/100.0
-dem_behav_data_v2.loc[:,cog_columns] = dem_behav_data_v2.loc[:, cog_columns]/100.0
+    #need to make cognitive numbers smaller or normative modeling function does not work
+    cog_columns = ['FlankerSU', 'DCSU', 'VocabSU', 'WMemorySU']
+    dem_behav_data_v1.loc[:,cog_columns] = dem_behav_data_v1.loc[:, cog_columns]/100.0
+    dem_behav_data_v2.loc[:,cog_columns] = dem_behav_data_v2.loc[:, cog_columns]/100.0
 
-#check for columns with nan values
-v1_has_nans = dem_behav_data_v1.isnull().sum()
-v2_has_nans = dem_behav_data_v2.isnull().sum()
+    #check for columns with nan values
+    v1_has_nans = dem_behav_data_v1.isnull().sum()
+    v2_has_nans = dem_behav_data_v2.isnull().sum()
 
-# Make n_splits train-test splits
+    # Make n_splits train-test splits
 
-df_for_train_test_split = dem_behav_data_v1.copy()
+    df_for_train_test_split = dem_behav_data_v1.copy()
 
-df_for_train_test_split['age_gender'] = (df_for_train_test_split['agegroup'].astype(str) + '_'
-                                         + df_for_train_test_split['gender'].astype(str))
+    df_for_train_test_split['age_gender'] = (df_for_train_test_split['agegroup'].astype(str) + '_'
+                                             + df_for_train_test_split['gender'].astype(str))
 
-# For ttsplit, create a dataframe that has only visit 1 data and only subject number, visit, age and sex as columns
-cols_to_keep = ['subject', 'visit', 'agegroup', 'gender', 'age_gender']
-cols_to_drop = [col for col in df_for_train_test_split if col not in cols_to_keep]
-df_for_train_test_split.drop(columns=cols_to_drop, inplace=True)
-# keep only the subjects that have data at both time points
-df_for_train_test_split = df_for_train_test_split[df_for_train_test_split['subject'].isin(all_subjects_2ts)]
+    # For ttsplit, create a dataframe that has only visit 1 data and only subject number, visit, age and sex as columns
+    cols_to_keep = ['subject', 'visit', 'agegroup', 'gender', 'age_gender']
+    cols_to_drop = [col for col in df_for_train_test_split if col not in cols_to_keep]
+    df_for_train_test_split.drop(columns=cols_to_drop, inplace=True)
+    # keep only the subjects that have data at both time points
+    df_for_train_test_split = df_for_train_test_split[df_for_train_test_split['subject'].isin(all_subjects_2ts)]
 
-# Initialize StratifiedShuffleSplit for equal train/test sizes
-splitter = StratifiedShuffleSplit(n_splits=n_splits, test_size=0.54, random_state=42)
+    # Initialize StratifiedShuffleSplit for equal train/test sizes
+    splitter = StratifiedShuffleSplit(n_splits=n_splits, test_size=0.54, random_state=42)
 
-train_set_list = []
-test_set_list = []
-# Perform the splits
-for i, (train_index, test_index) in enumerate(
-        splitter.split(df_for_train_test_split, df_for_train_test_split['age_gender'])):
-    train_set_list_tmp = df_for_train_test_split.iloc[train_index, 0].values.tolist()
-    train_set_list_tmp.extend(subjects_v1_only)
-    test_set_list_tmp = df_for_train_test_split.iloc[test_index, 0].values.tolist()
-    test_set_list_tmp.extend(subjects_v2_only)
-    train_set_list.append(train_set_list_tmp)
-    test_set_list.append(test_set_list_tmp)
+    train_set_list = []
+    test_set_list = []
+    # Perform the splits
+    for i, (train_index, test_index) in enumerate(
+            splitter.split(df_for_train_test_split, df_for_train_test_split['age_gender'])):
+        train_set_list_tmp = df_for_train_test_split.iloc[train_index, 0].values.tolist()
+        train_set_list_tmp.extend(subjects_v1_only)
+        test_set_list_tmp = df_for_train_test_split.iloc[test_index, 0].values.tolist()
+        test_set_list_tmp.extend(subjects_v2_only)
+        train_set_list.append(train_set_list_tmp)
+        test_set_list.append(test_set_list_tmp)
 
-train_set_array = np.array(list(train_set_list))
-test_set_array = np.array(list(test_set_list))
+    train_set_array = np.array(list(train_set_list))
+    test_set_array = np.array(list(test_set_list))
 
-fname_train = '{}/visit1_subjects_train_sets_{}_splits_{}.txt'.format(outputdirname, n_splits, struct_var)
-np.save(fname_train, train_set_array)
+    fname_train = '{}/visit1_subjects_train_sets_{}_splits_{}.txt'.format(outputdirname, n_splits, struct_var)
+    np.save(fname_train, train_set_array)
 
-fname_test = '{}/visit1_subjects_test_sets_{}_splits_{}.txt'.format(outputdirname, n_splits, struct_var)
-np.save(fname_test, test_set_array)
+    fname_test = '{}/visit1_subjects_test_sets_{}_splits_{}.txt'.format(outputdirname, n_splits, struct_var)
+    np.save(fname_test, test_set_array)
 
-# make directories to store files for model creation
-dirpath = os.path.join(outputdirname, 'data')
-try:
-    shutil.rmtree(dirpath)
-    print(f"Directory '{dirpath}' and its contents have been removed.")
-except FileNotFoundError:
-    print(f"Directory '{dirpath}' does not exist.")
-makenewdir('{}/data/'.format(outputdirname))
-makenewdir('{}/data/plots'.format(outputdirname))
+    # make directories to store files for model creation
+    dirpath = os.path.join(outputdirname, 'data')
+    try:
+        shutil.rmtree(dirpath)
+        print(f"Directory '{dirpath}' and its contents have been removed.")
+    except FileNotFoundError:
+        print(f"Directory '{dirpath}' does not exist.")
+    makenewdir('{}/data/'.format(outputdirname))
+    makenewdir('{}/data/plots'.format(outputdirname))
 
-# make file diretories for model testing
-dirpath = os.path.join(outputdirname, 'predict_files')
-try:
-    shutil.rmtree(dirpath)
-    print(f"Directory '{dirpath}' and its contents have been removed.")
-except FileNotFoundError:
-    print(f"Directory '{dirpath}' does not exist.")
-makenewdir('{}/predict_files/'.format(outputdirname))
-makenewdir('{}/predict_files/plots'.format(outputdirname))
+    # make file directories for model testing
+    dirpath = os.path.join(outputdirname, 'predict_files')
+    try:
+        shutil.rmtree(dirpath)
+        print(f"Directory '{dirpath}' and its contents have been removed.")
+    except FileNotFoundError:
+        print(f"Directory '{dirpath}' does not exist.")
+    makenewdir('{}/predict_files/'.format(outputdirname))
+    makenewdir('{}/predict_files/plots'.format(outputdirname))
 
-# Calculate Zscores for each response variable
+    # Calculate Zscores for each response variable
 
-Z_time1_behav, Z_time2_behav, behaviors = calculate_normative_model_behav(struct_var, dem_behav_data_v1, dem_behav_data_v2, train_set_array,
-                                                test_set_array,show_plots, spline_order, spline_knots, outputdirname,
-                                                n_splits)
+    Z_time1_behav, Z_time2_behav, behaviors = calculate_normative_model_behav(struct_var, dem_behav_data_v1, dem_behav_data_v2, train_set_array,
+                                                    test_set_array,show_plots, spline_order, spline_knots, outputdirname,
+                                                    n_splits)
 
-Z_time1_avg_allsplits = Z_time1_behav.groupby(by=['participant_id']).mean().drop(columns=['split'])
+    Z_time1_behav.to_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit1_{n_splits}_splits_allsplits.csv')
+    Z_time2_behav.to_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit2_{n_splits}_splits_allsplits.csv')
+
+Z_time1_behav = pd.read_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit1_{n_splits}_splits_allsplits.csv',
+                            usecols=lambda col: not col.startswith('Unnamed'))
+Z_time2_behav = pd.read_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit2_{n_splits}_splits_allsplits.csv',
+                            usecols=lambda col: not col.startswith('Unnamed'))
+
+Z_time1_avg_allsplits = Z_time1_behav.groupby(by=['subject']).mean()
 Z_time1_avg_allsplits.reset_index(inplace=True)
 
 Z_time2_avg_allsplits = Z_time2_behav.groupby(by=['participant_id']).mean().drop(columns=['split'])
 Z_time2_avg_allsplits.reset_index(inplace=True)
 
-calc_ef_stats(Z_time2_avg_allsplits)
+Z_time1_avg_allsplits.to_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit1_avg{n_splits}_splits.csv')
+Z_time2_avg_allsplits.to_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit2_avg{n_splits}_splits.csv')
+
+
+behaviors = ['FlankerSU', 'DCSU']
+
+reject_val, pvals_corr_both_genders = calc_ef_stats(Z_time2_avg_allsplits,outputdirname)
 
 plot_and_compute_zcores_by_gender(Z_time2_avg_allsplits, 'behavior', behaviors, outputdirname, n_splits)
-
-Z_time1_avg_allsplits.to_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit1_{n_splits}_splits.csv')
-Z_time2_avg_allsplits.to_csv(outputdirname+f'/Z_scores_all_meltzoff_cogn_behav_visit2_{n_splits}_splits.csv')
 
 plt.show()
 
--- calc_ef_stats.py
+++ calc_ef_stats.py
@@ -1,11 +1,77 @@
 import scipy.stats as stats
 import statsmodels.stats.multitest as smt
 import numpy as np
+from matplotlib import pyplot as plt
 
-def calc_ef_stats(z_time2):
+def calc_ef_stats(z_time2, working_dir):
+    z_time2 = z_time2.copy()
     z_time2 = z_time2.dropna()
     t_statistic_Flanker, p_value_Flanker = stats.ttest_1samp(z_time2['FlankerSU'], popmean=0, nan_policy='raise')
     t_statistic_DCCS, p_value_DCCS = stats.ttest_1samp(z_time2['DCSU'], popmean=0, nan_policy='raise')
 
     p_array = np.array([p_value_Flanker, p_value_DCCS])
-    reject_f, pvals_corrected, a1_f, a2_f = smt.multipletests(p_array, alpha=0.05, method='fdr_bh')
+    reject_val_allsubjs, pvals_corrected_allsubjs, a1_f, a2_f = smt.multipletests(p_array, alpha=0.05, method='fdr_bh')
+
+    # Add 'sex' column: 0 if participant_id even, 1 if odd
+    z_time2['sex'] = z_time2['participant_id'] % 2
+
+    # Split data by sex
+    flanker_male = z_time2.loc[z_time2['sex'] == 1, 'FlankerSU']
+    flanker_female = z_time2.loc[z_time2['sex'] == 0, 'FlankerSU']
+    dcsu_male = z_time2.loc[z_time2['sex'] == 1, 'DCSU']
+    dcsu_female = z_time2.loc[z_time2['sex'] == 0, 'DCSU']
+
+    # Run independent t-tests
+    t_flanker, p_flanker = stats.ttest_ind(flanker_male, flanker_female, nan_policy='omit')
+    t_dcsu, p_dcsu = stats.ttest_ind(dcsu_male, dcsu_female, nan_policy='omit')
+
+    # Collect p-values
+    pvals = [p_flanker, p_dcsu]
+
+    # Apply Benjamini-Hochberg FDR correction
+    reject, pvals_corrected, _, _ = smt.multipletests(pvals, alpha=0.05, method='fdr_bh')
+
+    # Print results
+    print(f"Sex effect Flanker p-value: {p_flanker:.2f}, corrected p-value: {pvals_corrected[0]:.2f}, significant: {reject[0]}")
+    print(f"Sex effect DCSU p-value: {p_dcsu:.2f}, corrected p-value: {pvals_corrected[1]:.2f}, significant: {reject[1]}")
+
+    def plot_distributions(data_male, data_female, measure_name):
+        combined_data = np.concatenate([data_male, data_female])
+
+        # Compute common bins across all data
+        num_bins = 20
+        min_val = np.min(combined_data)
+        max_val = np.max(combined_data)
+        bins = np.linspace(min_val, max_val, num_bins + 1)  # ensures consistent bin edges
+
+        fig, axes = plt.subplots(1, 2, figsize=(12,5), sharex=True)
+
+        # plot male vs female
+        axes[0].hist(data_female, bins=bins, alpha=1.0, label='female', color='crimson', edgecolor='black')
+        axes[0].hist(data_male, bins=bins, alpha=0.7, label='male', color='blue', edgecolor='black')
+        axes[0].axvline(x=0, color='k', linestyle='dotted', linewidth=1.5)
+        axes[0].set_title(f"{measure_name}: Male vs. Female")
+        axes[0].set_xlabel('Z-score')
+        axes[0].set_ylabel('Count')
+        handles, labels = axes[0].get_legend_handles_labels()
+        # Reorder the handles and labels
+        order = [1, 0]  # This assumes the 'female' is first and 'male' is second
+        axes[0].legend([handles[idx] for idx in order], [labels[idx] for idx in order], fontsize=10, loc='upper left')
+
+        # plot combined
+        axes[1].hist(combined_data, bins=20, alpha=0.5, label='All Subjects', color='gray', edgecolor='black')
+        axes[1].axvline(x=0, color='k', linestyle='dotted', linewidth=1.5)
+        axes[1].set_title(f"{measure_name}: All Subjects")
+        axes[1].set_xlabel('Z-score')
+        axes[1].set_ylabel('Count')
+
+        plt.tight_layout()
+
+        plt.savefig(f'{measure_name}_Distribution.png', dpi=300, bbox_inches='tight')
+
+        plt.show()
+
+    plot_distributions(flanker_male.values, flanker_female.values, 'Flanker')
+    plot_distributions(dcsu_male.values, dcsu_female.values, 'Dimensional Change Card Sort')
+
+    return reject_val_allsubjs, pvals_corrected_allsubjs
